77ddc878294a095064db29505ee8cb2ff8d7c99b
Induwara Kaveen <induwarakaveen44@gmail.com>
2026-08-29 14:19:49 +0530

    Updated FileOutputStrategy.java

--- src/output/FileOutputStrategy.java
+++ src/output/FileOutputStrategy.java
@@ -6,8 +6,12 @@ import java.io.IOException;
 public class FileOutputStrategy {
 
     public void output(String reportContent, String filePath) throws IOException {
-        if (filePath == null || filePath.isEmpty());
-        FileWriter writer = new FileWriter(filePath);
+        if (filePath == null || filePath.isEmpty()) {
+            throw new IllegalArgumentException("File path is required for file output method.");
+        }
+        try (FileWriter writer = new FileWriter(filePath)) {
+            writer.write(reportContent);
+        }
         System.out.println("Report successfully saved to " + filePath);
     }
 }
